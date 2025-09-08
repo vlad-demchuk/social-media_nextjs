89ec247e1876eb8929406c3253fc07c3039b3cd2
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-09-08 18:14:50 +0300

    Add debug log

--- src/features/posts/components/index.tsx
+++ src/features/posts/components/index.tsx
@@ -1,11 +1,17 @@
+'use client';
 import { query } from '@/lib/graphql/apolloClient';
 import { GET_POSTS } from '@/graphql/queries/post';
 import { List } from '@/features/posts/components/list';
+import { useQuery } from '@apollo/client/react';
 
-export default async function Posts() {
-  const { data: postsData, error } = await query({
-    query: GET_POSTS,
-  });
+export default function Posts() {
+  // const { data: postsData, error } = await query({
+  //   query: GET_POSTS,
+  // });
+
+  const { data: postsData, error, loading } = useQuery(GET_POSTS);
+
+  if (loading) return 'Loading';
 
   if (error || !postsData?.posts) return <>Something went wrong</>;
 
