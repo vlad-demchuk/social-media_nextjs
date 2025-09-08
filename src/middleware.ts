b0aca482e5927766aef727dc2901f368b69c1d4b
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-09-08 18:00:36 +0300

    Add debug log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { getSessionCookie } from 'better-auth/cookies';
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
+  console.log('>>>>> sessionCookie:', sessionCookie);
 
   // Check for the existence of a session cookie to handle redirection.
   // To avoid blocking requests by making API or database calls.
@@ -15,5 +16,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: 'nodejs',
-  matcher: [],
+  matcher: ['/feed'],
 };
