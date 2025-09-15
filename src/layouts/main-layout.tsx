7140b3637492396346421e2adcc15a538e3a88e3
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-09-15 21:09:02 +0300

    Refactor styles for sidebar

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -7,19 +7,24 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen grid grid-rows-[auto_1fr_auto] lg:grid-rows-[auto_1fr] bg-gradient-hero">
+    <div className="h-screen lg:min-h-screen lg:h-auto grid grid-rows-[auto_1fr_auto] lg:grid-rows-[auto_1fr] bg-gradient-hero">
       {/* Top Navigation */}
       <AppHeader />
 
-      <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 overflow-y-auto">
-        <div className="h-full grid grid-cols-1 lg:grid-cols-[30%_1fr] gap-6">
+      {/* Middle Zone */}
+      <div className="max-w-7xl row w-full mx-auto lg:px-8 overflow-y-auto lg:overflow-y-visible">
+        <div className="flex">
+          {/* Left Navigation */}
           <Navigation placement="left" />
-          <main className="lg:mb-0 col-span-1 space-y-6">
+
+          {/* Scrollable Feed */}
+          <main className="grow space-y-6 p-6">
             {children}
           </main>
         </div>
       </div>
 
+      {/* Bottom Navigation (mobile) */}
       <Navigation placement="bottom" />
     </div>
   );
